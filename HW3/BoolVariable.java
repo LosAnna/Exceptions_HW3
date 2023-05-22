7c9b94d882c198e64e6757289d20d18870214d4d
losanna <79831340359@yandex.ru>
2023-05-22 23:07:49 +0700

    HW 3

---
+++ HW3/BoolVariable.java
@@ -0,0 +1,22 @@
+package HW3;
+public class BoolVariable {
+
+    private boolean bool;
+    private String s;
+
+    BoolVariable() {
+    }
+
+    public boolean getBool() {
+        return bool;
+    }
+    public String getS() {
+        return s;
+    }
+    public void setBool(boolean bool) {
+        this.bool = bool;
+    }
+    public void setS(String s) {
+        this.s = s;
+    }
+}
